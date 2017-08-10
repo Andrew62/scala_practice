825bfc7a9d71773525dd006c3ef55fe4021c82f3
Andrew62 <awoizesko@gmail.com>
2017-08-10 17:00:55 +1000

    Added KNN alg -- that was an easy one

--- src/test/scala/trees/DecisionTreeTest.scala
+++ src/test/scala/trees/DecisionTreeTest.scala
@@ -6,8 +6,10 @@ import scala.util.Random.{nextFloat, nextInt}
 
 class DecisionTreeTest extends FlatSpec {
 
-  val rows = 100
-  val cols = 30
+  behavior of "DecisionTree"
+
+  val rows = 10
+  val cols = 3
   val X = new Matrix[Double](rows, cols)
   val y = new Matrix[Int](rows, 1)
   private def fillArray[A](a: Matrix[A], randFunc: () => A) = {
@@ -22,7 +24,7 @@ class DecisionTreeTest extends FlatSpec {
   fillArray[Int](y, f)
 
   "A DecisionBranch" should "be created and fit when given data" in {
-    val tree = new DecisionTree(X, y, 10, 2)
+    val tree = new DecisionTree(X, y, 10, 1)
     assert(tree.root.isInstanceOf[DecisionBranch[Int]])
   }
 
@@ -35,12 +37,9 @@ class DecisionTreeTest extends FlatSpec {
   "Accuracy" should "be higher than zero" in {
     val tree = new DecisionTree(X, y, 10, 2)
     val y_pred = tree.predict(X)
-    var count = 0
-    for (rowidx <- 0 until y.rows) {
-      if (y(rowidx)(0) == y_pred(rowidx)) {
-        count += 1
-      }
-    }
+    var count = (0 until y.rows)
+      .map(x => if (y(x)(0) == y_pred(0)) 1 else 0)
+      .sum
     val accuracy = count / y_pred.length.asInstanceOf[Double]
     println(s"Accuracy: $accuracy")
     assert(accuracy > 0)
