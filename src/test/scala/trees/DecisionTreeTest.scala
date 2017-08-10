d9e92baabc76c07b2f37cb2866ef21b8dc050ffe
Andrew62 <awoizesko@gmail.com>
2017-08-10 13:10:28 +1000

    removing some for loops in favor of functional operations

--- src/test/scala/trees/DecisionTreeTest.scala
+++ src/test/scala/trees/DecisionTreeTest.scala
@@ -6,8 +6,8 @@ import scala.util.Random.{nextFloat, nextInt}
 
 class DecisionTreeTest extends FlatSpec {
 
-  val rows = 5
-  val cols = 2
+  val rows = 100
+  val cols = 30
   val X = new Matrix[Double](rows, cols)
   val y = new Matrix[Int](rows, 1)
   private def fillArray[A](a: Matrix[A], randFunc: () => A) = {
@@ -22,18 +22,18 @@ class DecisionTreeTest extends FlatSpec {
   fillArray[Int](y, f)
 
   "A DecisionBranch" should "be created and fit when given data" in {
-    val tree = new DecisionTree(X, y, 10, 1)
+    val tree = new DecisionTree(X, y, 10, 2)
     assert(tree.root.isInstanceOf[DecisionBranch[Int]])
   }
 
   "Predictions" should "be made when we fit and predict" in {
-    val tree = new DecisionTree(X, y, 10, 1)
+    val tree = new DecisionTree(X, y, 10, 2)
     val y_pred = tree.predict(X)
     assert(y_pred.isInstanceOf[Array[Int]])
   }
 
   "Accuracy" should "be higher than zero" in {
-    val tree = new DecisionTree(X, y, 10, 1)
+    val tree = new DecisionTree(X, y, 10, 2)
     val y_pred = tree.predict(X)
     var count = 0
     for (rowidx <- 0 until y.rows) {
